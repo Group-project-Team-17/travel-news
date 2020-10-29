d28f8576c58dbfabfb09bd329ea0100098d57e9c
Muhammad Akbar Habiby Khalid <akbarhabiby@icloud.com>
2020-10-29 12:00:59 +0700

    Fixed Oauth, still searching for redirect issue

--- server/controllers/oauth-controller.js
+++ server/controllers/oauth-controller.js
@@ -1,4 +1,7 @@
 const axios = require('axios')
+const { User } = require('../models/')
+const bcrypt = require("../helpers/bcrypt")
+const jwt = require("../helpers/jwt")
 
 class OauthController {
     static async github(req, res, next) {
@@ -20,9 +23,28 @@ class OauthController {
                 }
             })
 
-            // ! Convert Ke JSON WEB TOKEN
+            const githubExistUser = {
+                email: getResult.data.email,
+                password: getResult.data.node_id.substring(0, 15)
+            }
+            const githubNewUser = {
+                email: getResult.data.email,
+                password: getResult.data.node_id.substring(0, 15),
+                full_name: getResult.data.full_name
+            }
 
-            res.status(200).json({ data: getResult.data }) // ! Balikin yg udah ke convert berisi email sama nama lengkap
+            const findUser = await User.findOne({ where: { email: githubExistUser.email }})
+            const pass = bcrypt.checkPassword(githubExistUser.password, findUser.password)
+
+            if (findUser && pass) {
+                const accessToken = jwt.signToken({ email: findUser.email, full_name: findUser.full_name })
+                res.status(200).json({ accessToken }) // * Kalo user nya ketemu, buat access Token yang berisi email + full_name
+            } else {
+                const createUser = await User.create(githubNewUser)
+                const accessToken = jwt.signToken({ email: createUser.email, full_name: createUser.full_name })
+                res.status(200).json({ accessToken }) // * Kalo user nya belum ketemu, buat access Token yang berisi email + full_name
+            }
+
         } catch (error) {
             next(error)
         }
