51d8d2f00ec966c4943155810bdb1c1f8f2b7350
akbarhabiby <akbarhabiby@icloud.com>
2020-10-30 01:47:48 +0700

    Fixed Github Oauth, fixed some bugs

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,6 +1,7 @@
 const { User } = require('../models/')
 const bcrypt = require("../helpers/bcrypt")
 const jwt = require("../helpers/jwt")
+const axios = require('axios')
 
 class UserController {
     static async postRegisterUser(req, res, next) {
@@ -32,6 +33,53 @@ class UserController {
             }else{
                 throw {name:'Email atau Password salah'}
             }
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    static async githubLogin(req, res, next) {
+        try {
+            const requestToken = req.query.code
+            const getAccessToken = await axios({
+                method: 'POST',
+                url: `${process.env.PRIV8_GITHUB_OAUTH_URL}?client_id=${process.env.PRIV8_GITHUB_CLIENT_ID}&client_secret=${process.env.PRIV8_GITHUB_CLIENT_SECRET}&code=${requestToken}`,
+            })
+            
+            const accessToken = getAccessToken.data.substring(getAccessToken.data.indexOf('=') + 1, getAccessToken.data.indexOf('&'))
+                
+
+            const getResult = await axios({
+                method: 'GET',
+                url: 'https://api.github.com/user',
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                }
+            })
+
+            const githubExistUser = {
+                email: getResult.data.email,
+                password: getResult.data.node_id.substring(0, 15)
+            }
+            const githubNewUser = {
+                email: getResult.data.email,
+                password: getResult.data.node_id.substring(0, 15),
+                full_name: getResult.data.name
+            }
+
+            const findUser = await User.findOne({ where: { email: githubExistUser.email }})
+            if (findUser) {
+                const pass = bcrypt.checkPassword(githubExistUser.password, findUser.password)
+                if (pass) {
+                    const accessToken = jwt.signToken({ email: findUser.email, full_name: findUser.full_name })
+                    res.status(200).json({ accessToken }) // * Kalo user nya ketemu, buat access Token yang berisi email + full_name
+                }
+            } else {
+                const createUser = await User.create(githubNewUser)
+                const accessToken = jwt.signToken({ email: createUser.email, full_name: createUser.full_name })
+                res.status(200).json({ accessToken }) // * Kalo user nya belum ketemu, buat access Token yang berisi email + full_name
+            }
+
         } catch (error) {
             next(error)
         }
--- server/routes/user.js
+++ server/routes/user.js
@@ -2,12 +2,11 @@ const router = require('express').Router()
 const UserController = require('../controllers/user-controller')
 const AppController = require('../controllers/app-controller')
 const Auth = require("../middlewares/authentication")
-const OauthController = require('../controllers/oauth-controller')
 
 //Menambahkan Authentikasi untuk route setelah login
 router.get('/', Auth.authentication, AppController.getIndex)
 router.post('/register', UserController.postRegisterUser)
 router.post('/login', UserController.postLoginUser)
-router.get('/github', OauthController.github)
+router.get('/githublogin', UserController.githubLogin)
 
 module.exports = router
